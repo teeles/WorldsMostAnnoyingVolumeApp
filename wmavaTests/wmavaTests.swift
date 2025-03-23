5e246bddc985647d290c063dabfd6bf77e48e69f
Thomas Eeles <tom@tlemail.me>
2025-03-23 16:09:42 +0000

    PUSH
    Pushed files

---
+++ wmavaTests/wmavaTests.swift
@@ -0,0 +1,17 @@
+//
+//  wmavaTests.swift
+//  wmavaTests
+//
+//  Created by Thomas Eeles on 23/03/2025.
+//
+
+import Testing
+@testable import wmava
+
+struct wmavaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
